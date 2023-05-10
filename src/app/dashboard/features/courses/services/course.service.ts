b9e9bbdf879c263434fb536ff0b66d700af76c00
Carolina Labastida Meza <carolina.labastida@nectri.com>
2023-05-09 22:39:01 -0600

    first commit

---
+++ src/app/dashboard/features/courses/services/course.service.ts
@@ -0,0 +1,93 @@
+import { Injectable } from '@angular/core';
+import { Course, formDataCourse } from '../models/course';
+import { BehaviorSubject, Observable, take, map } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environments';
+import { Student } from '../../students/models/student';
+import Swal from 'sweetalert2';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CourseService {
+  private courses$ = new BehaviorSubject<Course[] | null>(
+    null
+  );
+
+  constructor(private httpClient: HttpClient){
+
+  }
+
+  getCourses(): Observable<Course[] | null> {
+    this.httpClient.get<Course[]>(
+      `${environment.apiBaseUrl}/courses`
+    ).subscribe({
+      next: (courses) => {
+        this.courses$.next(courses);
+      },
+      complete: () => {},
+      error: () => {
+        return 'Ocurrió un error al obtener la información';
+      }
+    })
+
+    return this.courses$.asObservable();
+  }
+
+  getCourseById(id: number): Observable<Course | null>{
+    return this.httpClient.get<Course[]>(
+      `${environment.apiBaseUrl}/courses`, 
+      {
+        params: {
+          id: id
+        }
+      }
+    ).pipe(
+      map((courses) => courses[0])
+    )
+  }
+
+  createCourse(newCourse: formDataCourse): void{
+    this.httpClient.post<Student[]>(
+      `${environment.apiBaseUrl}/courses`, newCourse
+    ).subscribe({
+      next: () => {
+        this.getCourses();
+      },
+      complete: () => {},
+      error: () => {
+        Swal.fire('', 'Ocurrió un error al registrar el curso','error')
+      }
+    })
+  }
+
+  editCourse(courseId: number,modifiedCourse: formDataCourse): void{
+    this.httpClient.put<Student[]>(
+      `${environment.apiBaseUrl}/courses/${courseId}`, modifiedCourse
+    ).subscribe({
+      next: () => {
+        this.getCourses();
+      },
+      complete: () => {},
+      error: () => {
+        Swal.fire('', 'Ocurrió un error al modificar el curso', 'error')
+      }
+    })
+  }
+
+  deleteCourse(courseId: number): void {
+    this.httpClient.delete<Course[]>(
+      `${environment.apiBaseUrl}/courses/${courseId}`
+    ).subscribe({
+      next: () => {
+        this.getCourses();
+      },
+      complete: () => {},
+      error: () => {
+        Swal.fire('', 'Ocurrió un error al eliminar el curso', 'error')
+      }
+    })
+  }
+
+
+}
---
+++ src/app/dashboard/features/enrollments/services/enrollment.service.ts
@@ -0,0 +1,122 @@
+import { Injectable } from '@angular/core';
+import { Enrollment, formDataEnrollment } from '../models/enrollment';
+import { BehaviorSubject, Observable, map, take } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environments';
+import Swal from 'sweetalert2';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EnrollmentService {
+  enrollmentsCount: number = 0;
+
+
+  private enrollments$ = new BehaviorSubject<Enrollment[] | null>(
+    null
+  );
+
+  constructor(private httpClient: HttpClient){
+
+  }
+
+  getEnrollments(): Observable<Enrollment[] | null> {
+    this.httpClient.get<Enrollment[]>(
+      `${environment.apiBaseUrl}/enrollments`
+    ).subscribe({
+      next: (enrollments) => {
+        this.enrollments$.next(enrollments);
+      },
+      complete: () => {},
+      error: () => {
+        return 'Ocurrió un error al obtener la información';
+      }
+    })
+
+    return this.enrollments$.asObservable();
+  }
+
+  getEnrollmentById(id: number): Observable<Enrollment | null>{
+    return this.httpClient.get<Enrollment[]>(
+      `${environment.apiBaseUrl}/enrollments`, 
+      {
+        params: {
+          id: id
+        }
+      }
+    ).pipe(
+      map((enrollments) => enrollments[0])
+    )
+  }
+
+  createEnrollment(newEnrollment: formDataEnrollment): void{
+    this.httpClient.post<Enrollment[]>(
+      `${environment.apiBaseUrl}/enrollments`, 
+      {...newEnrollment, date: new Date()}
+    ).subscribe({
+      next: () => {
+        this.getEnrollments();
+      },
+      complete: () => {},
+      error: () => {
+        Swal.fire('', 'Ocurrió un error al registrar la inscripción','error')
+      }
+    })
+  }
+
+  editEnrollment(enrollmentId: number, modifiedEnrollment: formDataEnrollment): void{
+    this.httpClient.put<Enrollment[]>(
+      `${environment.apiBaseUrl}/enrollments/${enrollmentId}`, modifiedEnrollment
+    ).subscribe({
+      next: () => {
+        this.getEnrollments();
+      },
+      complete: () => {},
+      error: () => {
+        Swal.fire('', 'Ocurrió un error al modificar la inscripción', 'error')
+      }
+    })
+  }
+
+  deleteEnrollment(enrollmentId: number): void {
+    this.httpClient.delete<Enrollment[]>(
+      `${environment.apiBaseUrl}/enrollments/${enrollmentId}`
+    ).subscribe({
+      next: () => {
+        this.getEnrollments();
+      },
+      complete: () => {},
+      error: () => {
+        Swal.fire('', 'Ocurrió un error al eliminar la inscripción', 'error')
+      }
+    })
+  }
+
+  
+  getEnrollmentsByCourseId(id: number): Observable<Enrollment[]> {
+    return this.httpClient.get<Enrollment[]>(
+      `${environment.apiBaseUrl}/enrollments`, 
+      {
+        params: {
+          courseId: id
+        }
+      }
+    ).pipe(
+      map((enrollments) => enrollments)
+    )
+  }
+
+  getEnrollmentsByStudentId(id: number): Observable<Enrollment[]> {
+    return this.httpClient.get<Enrollment[]>(
+      `${environment.apiBaseUrl}/enrollments`, 
+      {
+        params: {
+          studentId: id
+        }
+      }
+    ).pipe(
+      map((enrollments) => enrollments)
+    )
+  }
+
+}
