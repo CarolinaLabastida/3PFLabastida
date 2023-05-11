6db28150a1fad6b0c00f2b6867d8ba18e6d203df
Carolina Labastida Meza <carolina.labastida@nectri.com>
2023-05-11 02:58:16 -0600

    unit tests

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { LoginFormValue } from 'src/app/core/models/user';
 
